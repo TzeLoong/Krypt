4c73c60e84db4f12f91858f3cacf9aa9751801a1
TzeLoong <tzeloong12@gmail.com>
2022-11-12 14:48:45 +0800

    Add all my files

---
+++ client/src/utils/constants.js
@@ -0,0 +1,5 @@
+import abi from './Transactions.json';
+
+
+export const contractABI = abi.abi;
+export const contractAddress = '0xA986290f03d537368E6249ad94FAd2573DDE2226';
---
+++ smart_contract/hardhat.config.js
@@ -0,0 +1,18 @@
+// https://eth-goerli.g.alchemy.com/v2/SxN1_Lv19-nCa_CVL-59lhx3JlytbyI-
+
+
+require('@nomiclabs/hardhat-waffle');
+
+module.exports = {
+  solidity: '0.8.0',
+  networks: {
+    goerli: {
+      url: 'https://eth-goerli.g.alchemy.com/v2/SxN1_Lv19-nCa_CVL-59lhx3JlytbyI-',
+      accounts: [ '52d530a9de07d33c2ae7310920bae3cbbd66ef49af37739e4577a35e46879fcc']
+    }
+
+  }
+}
+
+
+
